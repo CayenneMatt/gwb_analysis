7b8f24f81edc6f816ddae00608b5a6c1192eed45
Cayenne Matt <cayenne@umich.edu>
2026-02-18 10:40:19 -0500

    adding tests

--- tests/test_inits.py
+++ tests/test_inits.py
@@ -1,19 +1,17 @@
 # This test will import important modules and libraries.
-def test_imports():
-	import numpy as np
-	import scipy.stats as stats
 
-	import holodeck as holo
-	import holodeck.sams as sams
-	from holodeck.constants import MSOL
-	from holodeck import librarian
+from gwb_analysis.analyze_model import Model_Info
+import pytest
 
-	import kalepy as kale
-	import matplotlib as mpl
-	import matplotlib.pyplot as plt
-	from astropy.cosmology import WMAP9 as cosmo
-	from numpy import trapz
-	import astropy.units as u
-	import astropy.constants as c
+def test_model_info_posteriors():
+	path = '/Users/cayenne/Documents/Research/amplitude_paper/all_data/'
+	asa = Model_Info(path=path, file='ASA_noev.npz', model_name='Le11ne', color='b', line_style='-', threshold=2.5).get_posteriors()
+	# pytest.approx(asa.posteriors['hard_time'], float(3.5841389851321646))
+	assert asa.posteriors['hard_time'] == float(3.5841389851321646)
 
-	import copy
+
+def test_read_fits():
+	path = '/Users/cayenne/Documents/Research/amplitude_paper/all_data/'
+	asa = Model_Info(path=path, file='ASA_noev.npz', model_name='Le11ne', color='b', line_style='-', threshold=2.5)
+	fit = asa.get_shenf(0.2)
+	assert fit[1][0] == float(-2.241516079748106)
